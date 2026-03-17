8d6f81136fcb3a98e63a8f9f94dd6475b1bdb253
Abhijeet.M <abhijeetiyer07@gmail.com>
2026-03-17 18:15:15 +0530

    feat: implement ProtectedRoute wrapper and secure the /dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Landing from "./pages/landingPage.jsx";
 import Login from "./pages/LoginPage.jsx";
 import Signup from "./pages/SignupPage.jsx";
 import Dashboard from "./pages/DashboardPage.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx"; // 1. Added this import
 
 function App() {
   return (
@@ -11,7 +12,16 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Protected Route Group */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
